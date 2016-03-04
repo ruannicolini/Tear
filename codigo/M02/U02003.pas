35d7e77f09abec291dcbee60a033660b9e3e7b58
unknown <ruan_03@hotmail.com>
2016-03-04 19:24:34 -0300

    FMovimentacao e FTipoMovimentacao add

---
+++ codigo/M02/U02003.pas
@@ -0,0 +1,34 @@
+unit U02003;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, UBase, Data.DB, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
+  FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt,
+  System.Actions, Vcl.ActnList, System.ImageList, Vcl.ImgList,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client, Datasnap.Provider,
+  Datasnap.DBClient, Vcl.Buttons, Vcl.Grids, Vcl.DBGrids, DBGridBeleza,
+  Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.ComCtrls;
+
+type
+  TF02003 = class(TFBase)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  F02003: TF02003;
+
+implementation
+
+{$R *.dfm}
+
+Initialization
+  RegisterClass(TF02003);
+Finalization
+  UnRegisterClass(TF02003);
+end.
