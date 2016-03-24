751219c663a5cf8b3552609cf4b14ac55fb1eea6
unknown <ruan_03@hotmail.com>
2016-03-24 10:23:06 -0300

    F02004 add

---
+++ codigo/M02/U02004.pas
@@ -0,0 +1,35 @@
+unit U02004;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, UBase, Data.DB, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
+  FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt,
+  System.Actions, Vcl.ActnList, System.ImageList, Vcl.ImgList,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client, Datasnap.Provider,
+  Datasnap.DBClient, Vcl.Buttons, Vcl.Grids, Vcl.DBGrids, DBGridBeleza,
+  Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.ComCtrls;
+
+type
+  TF02004 = class(TFBase)
+    Panel3: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  F02004: TF02004;
+
+implementation
+
+{$R *.dfm}
+
+Initialization
+  RegisterClass(TF02004);
+Finalization
+  UnRegisterClass(TF02004);
+end.
